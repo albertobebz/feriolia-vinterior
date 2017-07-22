73f260d04998d0e8e959cc8096646a551641c97c
Alberto~Ferioli <alberto@mumsnet.com>
2017-07-22 14:07:22 +0100

    create scan method for checkout class attached to prototype

--- vinterior-js-test.js
+++ vinterior-js-test.js
@@ -8,6 +8,25 @@ Checkout.prototype = {
 
   add: function(itemCost) {
     this.subTotal += itemCost;
-  }
+  },
+
+  scan: function(item, qty) {
+    var qty = qty || 1;
+    for(var i = 0; i < qty; i++) {
+      this.basket.push(item);
+    }
+    if(item === "001"){ qty >= 2 ? this.add(8.50 * qty) : this.add(9.25 * qty); }
+    else if(item === "002"){this.add(45.00 * qty)}
+    else if(item === "003"){this.add(19.95 * qty)}
+    else{console.log('Item not found')};
+  },
 
 }
+
+// ****USE OF THE INTERFACE**** //
+
+// Create a 'co' instance
+var co = new Checkout('promotionType');
+// Scan items
+co.scan('001');
+co.scan('002', 2);
